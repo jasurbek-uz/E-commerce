4c4d4204270c4ed079ab94677b8f584058af3f8c
Jasurbek Ergashaliev <braveredmi@gmail.com>
2025-01-28 09:36:42 +0900

    feat: server set up

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,7 @@
 console.log("Hello");
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+mongoose.connect(process.env.MONGO_URL as string, {})
+  .then((data) => { console.log("MongooDb connection succeed"); const PORT = process.env.PORT ?? 3003; })
+  .catch((err)=> console.log("Error connecting to MongoDb"))
